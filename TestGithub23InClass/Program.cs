b3f4254e13194aca7ca0a3a223e988f6342b35c6
Poul Henriksen <pohe@zealand.dk>
2023-04-21 12:35:02 +0200

    LAvet udprint i luop sprint 1

--- TestGithub23InClass/Program.cs
+++ TestGithub23InClass/Program.cs
@@ -6,3 +6,5 @@ Console.WriteLine("Hello, World!");
 ClassPohe cp = new ClassPohe("POHE");
 Console.WriteLine(cp.Name);
 Console.WriteLine("Sprint 1");
+
+Console.WriteLine("Lavet af Luop i sprint 1");

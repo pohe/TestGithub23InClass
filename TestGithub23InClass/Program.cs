ca644cb9acca04304929cf5b5d8bda909abccf64
Poul Henriksen <pohe@zealand.dk>
2023-04-21 12:35:56 +0200

    udskrift af POhe sprint 1

--- TestGithub23InClass/Program.cs
+++ TestGithub23InClass/Program.cs
@@ -6,3 +6,5 @@ Console.WriteLine("Hello, World!");
 ClassPohe cp = new ClassPohe("POHE");
 Console.WriteLine(cp.Name);
 Console.WriteLine("Sprint 1");
+
+Console.WriteLine("LAvet af POHE i Sprint 1");

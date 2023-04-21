510bf601ee4c9647108a9fab8cc1ac774b67da99
Poul Henriksen <pohe@zealand.dk>
2023-04-21 11:13:02 +0200

    LAvet instans af ClassLuop

--- TestGithub23InClass/Program.cs
+++ TestGithub23InClass/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using TestGithub23InClass;
+
 Console.WriteLine("Hello, World!");
+
+
+
+ClassLuop lu = new ClassLuop("LUOP","vej 123");
+Console.WriteLine(lu.Name + " " + lu.Address);

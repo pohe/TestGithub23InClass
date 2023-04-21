fec8ec93452b7e0dd02a37c717063e73cab2b3df
Poul Henriksen <pohe@zealand.dk>
2023-04-21 10:58:53 +0200

    LAvet ClassLuop

---
+++ TestGithub23InClass/ClassLuop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGithub23InClass
+{
+    public class ClassLuop
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public ClassLuop(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+    }
+}
